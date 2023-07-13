748af5e49f9b4856464e2c2724cda601811385a2
Alexander Dang <alexdang1993374@gmail.com>
2023-07-12 19:25:53 -0700

    feat: responsive

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -23,15 +23,19 @@ const BackgroundSt = styled.div`
   background-image: ${sunsetBackground};
   background-size: 400% 400%;
   animation: ${movingGradient} 25s ease infinite;
-  padding: 2rem 5rem 14rem 5rem;
+  padding: 2rem 0rem 14rem 0rem;
   height: 21rem;
   width: 100%;
 
-  @media screen and (max-width: 767px) {
-    height: 42rem;
+  @media screen and (max-width: 830px) {
+    height: 15rem;
   }
 
-  @media screen and (max-width: 515px) {
-    height: 46rem;
+  @media screen and (max-width: 620px) {
+    height: 7rem;
+  }
+
+  @media screen and (max-width: 460px) {
+    height: 1rem;
   }
 `;
--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -3,6 +3,7 @@ import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import { hero, mamasan, promoBanner } from "@/assets";
+import styled from "styled-components";
 
 const images: StaticImageData[] = [promoBanner, hero, mamasan];
 
@@ -10,17 +11,29 @@ const ImageCarousel = () => {
   return (
     <Carousel autoPlay infiniteLoop showStatus={false}>
       {images.map((image, index) => (
-        <div key={index}>
+        <ImageContainer key={index}>
           <Image
             src={image}
             alt={`Carousel ${index}`}
             width={1000}
             height={300}
           />
-        </div>
+        </ImageContainer>
       ))}
     </Carousel>
   );
 };
+
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
 
 export default ImageCarousel;
